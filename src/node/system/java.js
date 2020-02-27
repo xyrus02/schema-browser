74c923e51fecaf5cad6d9c0b2ef32a029f15fc93
Georg Kiehne <georg.kiehne@xyrus-worx.org>
2020-02-27 22:32:49 +0100

    Added dependency and changed Java loader to respect dependencies

--- src/node/system/java.js
+++ src/node/system/java.js
@@ -1,8 +1,16 @@
 "use strict";
 
-const java = require('java');
+const fs = require("fs");
+const java = require("java");
+const baseDir = "./dependency";
+const dependencies = fs.readdirSync(baseDir);
+
+dependencies.forEach(function(dependency){
+    java.classpath.push(baseDir + "/" + dependency);
+});
 
 java.classpath.push("./classes");
+
 exports.getJavaInstance = function() {
     return java;
 };
